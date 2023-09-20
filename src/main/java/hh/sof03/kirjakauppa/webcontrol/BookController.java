efd13545763dcb162d5a6ef4141bb5a148a02f62
JouniAUT <jouni.au.tuomela@gmail.com>
2023-09-20 14:49:40 +0300

    Kolmas päivitys

--- src/main/java/hh/sof03/kirjakauppa/webcontrol/BookController.java
+++ src/main/java/hh/sof03/kirjakauppa/webcontrol/BookController.java
@@ -3,8 +3,11 @@ package hh.sof03.kirjakauppa.webcontrol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.sof03.kirjakauppa.domain.Book;
 import hh.sof03.kirjakauppa.domain.BookRepository;
 
 @Controller
@@ -12,12 +15,31 @@ public class BookController {
 	
 	@Autowired
 	BookRepository bookRepository;
+	
 
 	
-	@GetMapping("/index")
+	/*@RequestMapping(value = "index")
 	String showIndex(Model model) {
-		
 		model.addAttribute("books", bookRepository.findAll());
 		return "index";
+	}*/
+	
+	@RequestMapping(value = "/booklist")
+	public String bookList(Model model) {
+		model.addAttribute("books", bookRepository.findAll());
+		return "booklist";
 	}
+	
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		bookRepository.save(book);
+		return "redirect:booklist";
+	}
+	
 }
